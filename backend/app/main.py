d8efe9bbda73c81ccb61ed6e34914cd330d15415
MUSKAN4946 <muskanverma.7676@gmail.com>
2026-07-16 18:29:50 +0530

    Phase 4 - PostgreSQL Database Integration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,33 @@
 from fastapi import FastAPI
+
 from app.config.settings import PROJECT_NAME, PROJECT_VERSION
+from app.database.database import engine
 
 app = FastAPI(
     title=PROJECT_NAME,
     version=PROJECT_VERSION
 )
 
+
 @app.get("/")
 def home():
     return {
         "message": f"Welcome to {PROJECT_NAME}"
     }
+
+
+@app.get("/database")
+def database_check():
+    try:
+        connection = engine.connect()
+        connection.close()
+
+        return {
+            "status": "Database Connected Successfully"
+        }
+
+    except Exception as e:
+        return {
+            "status": "Connection Failed",
+            "error": str(e)
+        }
