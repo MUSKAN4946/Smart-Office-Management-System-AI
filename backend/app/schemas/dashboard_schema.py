7a77520d2ded6fa4437846c4d3fbfd0c0f98357b
MUSKAN4946 <muskanverma.7676@gmail.com>
2026-08-13 21:57:42 +0530

    Completed Phase 35 - Advanced Dashboard Module

--- backend/app/schemas/dashboard_schema.py
+++ backend/app/schemas/dashboard_schema.py
@@ -3,8 +3,20 @@ from pydantic import BaseModel
 
 class DashboardResponse(BaseModel):
     total_employees: int
+    active_employees: int
+    inactive_employees: int
+
     total_departments: int
+
     total_attendance: int
+    present_today: int
+    absent_today: int
+
     total_leaves: int
+    pending_leaves: int
+    approved_leaves: int
+    rejected_leaves: int
+
     total_payrolls: int
+
     total_users: int
--- backend/app/services/dashboard_service.py
+++ backend/app/services/dashboard_service.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from sqlalchemy.orm import Session
 
 from app.models.employee import Employee
@@ -8,13 +10,49 @@ from app.models.payroll import Payroll
 from app.models.user import User
 
 
+
 def get_dashboard_data(db: Session):
 
     return {
         "total_employees": db.query(Employee).count(),
+
+        "active_employees": db.query(Employee).filter(
+            Employee.is_active == True
+        ).count(),
+
+        "inactive_employees": db.query(Employee).filter(
+            Employee.is_active == False
+        ).count(),
+
         "total_departments": db.query(Department).count(),
+
         "total_attendance": db.query(Attendance).count(),
+
+        "present_today": db.query(Attendance).filter(
+            Attendance.attendance_date == date.today(),
+            Attendance.status == "Present"
+        ).count(),
+
+        "absent_today": db.query(Attendance).filter(
+            Attendance.attendance_date == date.today(),
+            Attendance.status == "Absent"
+        ).count(),
+
         "total_leaves": db.query(Leave).count(),
+
+        "pending_leaves": db.query(Leave).filter(
+            Leave.status == "Pending"
+        ).count(),
+
+        "approved_leaves": db.query(Leave).filter(
+            Leave.status == "Approved"
+        ).count(),
+
+        "rejected_leaves": db.query(Leave).filter(
+            Leave.status == "Rejected"
+        ).count(),
+
         "total_payrolls": db.query(Payroll).count(),
+
         "total_users": db.query(User).count()
     }
